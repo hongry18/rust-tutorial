cdab18903d6b745aad8a34af95bcd15ff66d4b51
hongry <hongry@hongryui-MacBookPro.local>
2021-04-05 10:15:26 +0900

    rust first commit
    chapter 1 진행중

---
+++ 01.Hello-World/comments.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // This is an example of a line comment
+
+    /*
+     * This is another type of comment, a block comment.
+     */
+
+    /*
+    Note: The previous column of `*` was entriely for style. There's no actual need for it.
+    */
+
+    let x = 5 + /* 90 + */ 5;
+    println!("Is 'x' 10 or 100? x = {}", x)
+}
